f0e158758cfce3e63cb27609e019f20667476062
Tony <864741831@qq.com>
2018-10-13 13:41:51 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ContentProviderDemo
 详细请看 [简书](http://www.jianshu.com/p/c0f38b284743)
+
+https://www.cnblogs.com/labixiaoxin/p/6340121.html
--- app/src/main/java/com/luffy/contentproviderdemo/MainActivity.java
+++ app/src/main/java/com/luffy/contentproviderdemo/MainActivity.java
@@ -1,11 +1,16 @@
 package com.luffy.contentproviderdemo;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,6 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnContacts=(Button)findViewById(R.id.btn_contacts);
         btnContacts.setOnClickListener(this);
 
+        for (int i = 0; i < 1000; i++){
+            addContact("zhangphil"+i, "12345678901");
+        }
+
     }
 
 
@@ -38,4 +47,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
+
+
+
+
+
+
+
+
+    // 一个添加联系人信息的例子
+    public void addContact(String name, String phoneNumber) {
+        // 创建一个空的ContentValues
+        ContentValues values = new ContentValues();
+
+        // 向RawContacts.CONTENT_URI空值插入，
+        // 先获取Android系统返回的rawContactId
+        // 后面要基于此id插入值
+        Uri rawContactUri = getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
+        long rawContactId = ContentUris.parseId(rawContactUri);
+        values.clear();
+
+        values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
+        // 内容类型
+        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
+        // 联系人名字
+        values.put(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME, name);
+        // 向联系人URI添加联系人名字
+        getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+        values.clear();
+
+        values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
+        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        // 联系人的电话号码
+        values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, phoneNumber);
+        // 电话类型
+        values.put(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE);
+        // 向联系人电话号码URI添加电话号码
+        getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+        values.clear();
+
+        values.put(ContactsContract.Data.RAW_CONTACT_ID, rawContactId);
+        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE);
+        // 联系人的Email地址
+        values.put(ContactsContract.CommonDataKinds.Email.DATA, "zhangphil@xxx.com");
+        // 电子邮件的类型
+        values.put(ContactsContract.CommonDataKinds.Email.TYPE, ContactsContract.CommonDataKinds.Email.TYPE_WORK);
+        // 向联系人Email URI添加Email数据
+        getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
+
+        Toast.makeText(this, "联系人数据添加成功", Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
